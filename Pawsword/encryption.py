9081471b4fdc7dfaa6e297d52e5362974d76ca93
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-12-22 10:18:40 -0500

    Finished vault decryption

--- Pawsword/encryption.py
+++ Pawsword/encryption.py
@@ -3,7 +3,7 @@ from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 import os
 import json
 
-def encrypt_vault(email: str,masterpass: str,json_to_encrypt: dict):
+def encrypt_vault(email: str,masterpass: str,json_to_encrypt: dict) -> bytes:
     vault_key = f"{email}|{masterpass}".encode()
 
     salt = os.urandom(16) # makes random 16 bytes of data
@@ -29,9 +29,25 @@ def encrypt_vault(email: str,masterpass: str,json_to_encrypt: dict):
 
     return salt + nonce + encryped_data
 
-def decrypt_vault(email: str,masterpass: str,full_encrypted_data: bytes):
+def decrypt_vault(email: str,masterpass: str,full_encrypted_data: bytes) -> dict:
     salt = full_encrypted_data[:16] # first 16 bytes is salt
     nonce = full_encrypted_data[16:28] # next 12 bytes is nonce
     encrypted_data = full_encrypted_data[28:] # everything else
 
-    
+    vault_key = f"{email}|{masterpass}".encode()
+
+    kdf = Scrypt( # get same kdf
+        salt=salt,
+        length=32,
+        n=2**14,
+        r=8,
+        p=1 
+    )
+
+    key = kdf.derive(vault_key) # get same vault key
+
+    aesgcm = AESGCM(key) # get same AESGCM
+
+    decrypted_data = aesgcm.decrypt(nonce,encrypted_data,associated_data=None)
+
+    return json.loads(decrypted_data.decode()) # returns as json
