6a2d577771b855284622d6467d8f2a9513d52b7f
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-12-22 13:05:40 -0500

    Add favicon

--- Pawsword/ui/app.py
+++ Pawsword/ui/app.py
@@ -2,13 +2,30 @@ import customtkinter as ctk
 from Pawsword.control import load_vault, vault_exists
 from Pawsword.ui.frames.login_f import LoginFrame
 from Pawsword.ui.frames.vault_f import VaultFrame
-
+from tkinter import PhotoImage
+import os
+import sys
 
 app = ctk.CTk()
 
 app.geometry('900x700')
 
+app.resizable(False, False)
+
 app.title('Pawsword')
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if sys.platform == "win32":
+    icon_path = os.path.join(current_dir, "../assets/cat-hacker.ico")
+    icon_path = os.path.normpath(icon_path)
+    if os.path.exists(icon_path):
+        app.iconbitmap(icon_path)
+else:
+    icon_path = os.path.join(current_dir, "../assets/cat-hacker.png")
+    icon_path = os.path.normpath(icon_path)
+    if os.path.exists(icon_path):
+        icon = PhotoImage(file=icon_path)
+        app.iconphoto(True, icon)
 
 app.frames = {}
 
