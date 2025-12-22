45dc02c13be13e741a0cf1fb5c76dba656943efc
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-12-22 16:19:11 -0500

    Add command line interface for managing password entries

---
+++ Pawsword/cli/command_line.py
@@ -0,0 +1,60 @@
+from Pawsword.control import add_entry, get_entry, vault_exists, list_services, remove_entry
+import typer
+from getpass import getpass
+from cryptography.exceptions import InvalidTag
+
+app = typer.Typer()
+
+session = {"email": None, "masterpass": None}
+
+def login():
+
+    if not vault_exists():
+        typer.echo("Vault does not exist")
+        return typer.Exit()
+
+    typer.echo("--------------")
+
+    session["email"] = typer.prompt("Email")
+    session["masterpass"] = getpass("Masterpass: ")
+
+    typer.echo("--------------")
+
+def require_login():
+    if not session["email"] or not session["masterpass"]:
+        login()
+
+@app.command()
+def add(service: str):
+    require_login()
+
+    username = typer.prompt(f"Username for {service}")
+    password = getpass(f"Password for {service}: ")
+
+    try:
+        add_entry(session["email"],session["masterpass"],service,username,password)
+        typer.secho(f"Added service {service}", fg=typer.colors.GREEN)
+    except InvalidTag:
+        typer.secho("Incorrect login!", fg=typer.colors.RED)
+    except ValueError:
+        typer.secho("Service already exists!", fg=typer.colors.RED)
+    except Exception as e:
+        typer.secho(f"Failed to add service: {e}", fg=typer.colors.RED)
+
+@app.command()
+def remove(service: str):
+    require_login()
+
+    try:
+        remove_entry(session["email"],session["masterpass"],service)
+        typer.secho(f"Removed service {service}", fg=typer.colors.GREEN)
+    except InvalidTag:
+        typer.secho("Incorrect login!", fg=typer.colors.RED)
+    except ValueError:
+        typer.secho("Service does not exist!", fg=typer.colors.RED)
+    except Exception as e:
+        typer.secho(f"Failed to add service: {e}", fg=typer.colors.RED)
+
+
+def main():
+    app()
